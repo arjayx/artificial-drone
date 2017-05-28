23c3cc3eba3f85ec9c4eff3524fa18236c81ee8a
roberto daza castrp <arjayx2011@gmail.com>
2017-05-27 21:33:10 -0500

    morphologi operation developed

--- prueba2.py
+++ prueba2.py
@@ -3,40 +3,53 @@ import cv2
 print "Programa iniciado"
 
 
-def clean_errors(image):
+def clean_errors(image,operation):
 	tam = image.shape
-	status = 0
+	# erode
+	if(operation == 1):
+		center = 0
+		status = 255
+		variable = 0
+	#dilate
+	else:
+		center = 255
+		status = 0
+		variable = 255
+	iteration = 3
 	for i in range(0, tam[0]):
 		for j in range(0, tam[1]):
-			if(image[i][j] == 255):
+			if(image[i][j] == center):
 				if (i>0 and image[i-1][j]==status): 
-					image[i-1][j] = 2
+					for k in range(1, iteration):			
+						if(i-k > 0):
+							image[i-k][j] = 2
 				
 				if (j>0 and image[i][j-1]==status): 
-					image[i][j-1] = 2
+					for k in range(1, iteration):
+						if(j-k > 0):
+							image[i][j-k] = 2
 				
 				if (i+1< tam[0] and image[i+1][j]==status): 
-					image[i+1][j] = 2
+					for k in range(1, iteration):
+						if(i+k < tam[0]):
+							image[i+k][j] = 2
 				
 				if (j+1<tam[1] and image[i][j+1]==status): 
-					image[i][j+1] = 2
-	
-	return image
-
-def morfology(frame, variable):
-	tam = frame.shape
+					for k in range(1, iteration):
+						if(j+k < tam[1]):
+							image[i][j+k] = 2
+				#image[i][j] = 2
 
 	for i in range(0, tam[0]):
 		for j in range(0, tam[1]):
-			if(frame[i][j] == 2):
-				frame[i][j] = variable
-	return frame
-
+			if(image[i][j] == 2):
+				image[i][j] = variable	
+	return image
 
 frames = []
 i = 1
 acum = np.zeros((720, 1280))
-frame2 = cv2.imread('frame200.jpg', 0)
+frame2 = cv2.imread('frame500.jpg', 0)
 while(i <= 100):
 	frame = cv2.imread('frame' + str(i) + '.jpg', 0)
 	frames.append(frame)
@@ -52,8 +65,7 @@ ther = 30
 max_value = 255
 th, dst = cv2.threshold(difference,ther, max_value, cv2.THRESH_BINARY);
 cv2.imwrite('salida4.jpg', dst)
-new_frame = clean_errors(dst)
-morfo = morfology(new_frame, 255)
+morfo = clean_errors(dst, 1)
 cv2.imwrite('morfo.jpg', morfo)
 
 #difference = frame2 - frame1
